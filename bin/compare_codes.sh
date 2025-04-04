b801e16fca177133e2b92127f08c75f34f44c88f
Chris Roeder <47036053+chrisroederucdenver@users.noreply.github.com>
2025-04-03 20:35:05 -0600

    cleaned down to essentials for moving to transform. Probably shouldn't continue development from here as a jupyter workspace

--- bin/compare_codes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-for file in snooper_output/*; do
-    base_file=$(basename "$file")
-    diff "snooper_output/$base_file" "tools_correct_output/$base_file"
-    if (( $? > 0 ))
-    then
-        echo "diff failed for $file with status $?"
-    fi
-done
